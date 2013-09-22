6b428df1b138f0d66a93d86cd7ef4d960d6ad6c2
John Wang <jwang@cpan.org>
2013-09-22 18:55:49 +0000

    Import of JWANG/Encode-VN-0.06 from CPAN.
    gitpan-cpan-distribution: Encode-VN
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         JWANG/Encode-VN-0.06.tar.gz
    gitpan-cpan-author:       JWANG
    gitpan-cpan-maturity:     released

--- lib/Encode/VN.pm
+++ lib/Encode/VN.pm
@@ -1,6 +1,6 @@
 package Encode::VN;
 use 5.007003;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 use Encode;
 use XSLoader;
 XSLoader::load(__PACKAGE__,$VERSION);
@@ -10,7 +10,6 @@ Encode::define_alias( qr/\bVNI-ASCII$/i            => '"x-viet-vni-ascii"' );
 Encode::define_alias( qr/\bVNI-Mac$/i              => '"x-viet-vni-mac"' );
 Encode::define_alias( qr/\bVNI-Email$/i            => '"x-viet-vni-email"' );
 Encode::define_alias( qr/\bVPS$/i                  => '"x-viet-vps"' );
-Encode::define_alias( qr/\bX-Viet-TCVN5712-1993$/i => '"x-viet-tcvn5712"' );
 
 1;
 __END__
@@ -21,7 +20,7 @@ Encode::VN - Extra sets of Vietnamese encodings
 
 =head1 VERSION
 
-This document describes version 0.05 of Encode::VN, released September 15, 2013.
+This document describes version 0.06 of Encode::VN, released September 15, 2013.
 
 =head1 SYNOPSIS
 
@@ -52,15 +51,12 @@ This version includes the following encoding tables:
   x-viet-vni-mac   /\bVNI-Mac$/i               VNI Mac
   x-viet-vni-email /\bVNI-Email$/i             VNI Internet Mail (Win/Unix/Mac)
   x-viet-vps       /\bVPS$/i                   Vietnamese Professionals Society
-  x-viet-tcvn5712  /\bX-Viet-TCVN5712-1993$/i  TCVN 5712:1993
 
 =head1 SEE ALSO
 
 Vietnamese Unicode SourceForge project: L<http://vietunicode.sourceforge.net/>
 
 VNI Wikipedia page: L<http://en.wikipedia.org/wiki/VNI>
-
-Vietnam Standards TCVN Wikipedia page: L<http://en.wikipedia.org/wiki/Vietnam_Standards>
 
 Mozilla VPS mappings: L<http://lxr.mozilla.org/seamonkey/source/intl/uconv/ucvlatin/vps.uf>,
                       L<http://lxr.mozilla.org/seamonkey/source/intl/uconv/ucvlatin/vps.ut>
@@ -70,11 +66,11 @@ L<Encode>
 =head1 ACKNOWLEDGEMENTS
 
 Maps for C<VNI> are generated from the F<vnichar.htm> file,
-courtesy of the VNI Sofware Comany, L<http://vnisoft.com/english/vnichar.htm>).
+courtesy of the VNI Sofware Comany, L<http://vnisoft.com/english/vnichar.htm>.
 
-Maps for C<VPS> and C<TCVN5712> are generated from the "Unicode & Vietnamese
-Legacy Character Encodings" page courtesy of the Vietnamese Unicode project
-on SourceForge, L<http://vietunicode.sourceforge.net/charset/>.
+Map for C<VPS> is generated from the "Unicode & Vietnamese Legacy Character
+Encodings" page courtesy of the Vietnamese Unicode project on SourceForge,
+L<http://vietunicode.sourceforge.net/charset/>.
 
 =head1 AUTHORS
 
